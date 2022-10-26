f598a9600137891dd70c47ec41daaac0bb29bfe2
Musabbir-pr <musabbir313@gmail.com>
2022-10-27 01:39:26 +0600

    added courses and api

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express= require('express')
 const app= express()
 const port= process.env.PORT || 5000;
+const cors=require('cors')
+app.use(cors())
 
+const categories= require('./data/courses.json')
 app.get('/', (req,res)=>{
-    
+    res.send('check the api to load data')
+})
+
+app.get("/courses", (req, res) => {
+    res.send(categories);
+  });
+
+  app.get('/courses')
+
+app.listen(port, ()=>{
+    console.log('server running', port);
 })

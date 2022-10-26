2f5fc57200532ff6328a3d5f5152755176964754
Musabbir-pr <musabbir313@gmail.com>
2022-10-27 04:57:40 +0600

    added dynamic api for coursedetails

--- index.js
+++ index.js
@@ -4,16 +4,27 @@ const port= process.env.PORT || 5000;
 const cors=require('cors')
 app.use(cors())
 
-const categories= require('./data/courses.json')
+const coursedetails= require('./data/coursedetails.json')
+const courses= require('./data/courses.json')
+
 app.get('/', (req,res)=>{
     res.send('check the api to load data')
 })
 
-app.get("/courses", (req, res) => {
-    res.send(categories);
+app.get("/allcourses", (req, res) => {
+    res.send(courses);
   });
 
-  app.get('/courses')
+  app.get("/courses", (req,res)=>{
+    res.send(coursedetails)
+  })
+
+  app.get('/courses/:id', (req, res)=>{
+    const id = req.params.id 
+    const singleCourse= coursedetails.find(course=>course.id===id)
+    console.log(req.params.id);
+    res.send(singleCourse)
+  })
 
 app.listen(port, ()=>{
     console.log('server running', port);
